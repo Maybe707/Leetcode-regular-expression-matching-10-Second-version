07a226dd3d5264b1b77455d6f911341f8eb64e80
Scream9 <fellfrostqwt@gmail.com>
2025-09-13 12:12:03 +0900

    Another one logic emplemented

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use std::{thread::current, time::Duration};
 struct Solution {
 }
 
+#[derive(Debug, Clone)]
+struct RegexpSymbol {
+	symbol_type: Vec<char>,
+	symbols: Vec<char>,
+	min: usize,
+	max: usize
+}
+
 impl Solution {
 	pub fn is_symbols_correct( current_p_symbol: char, current_s_symbol: char ) -> bool {
 		if (current_p_symbol.is_ascii_alphabetic() &&
@@ -19,26 +27,42 @@ impl Solution {
 			}
 	}
 
-	pub fn shift_back_matched_symbols_to_first_after_asterisk ( p_iterator: &mut usize, s_iterator: &mut usize,
-																consumed_symbols_number_after_asterisk:
-																&mut usize, is_no_match: bool ) {
-		if is_no_match {
-			// println!("shift back");
-			// println!("consumed: {}", consumed_symbols_number_after_asterisk);
-			*p_iterator -= *consumed_symbols_number_after_asterisk;
-			*s_iterator -= *consumed_symbols_number_after_asterisk;
-		}
-	}
-	
 	pub fn is_match(s: String, p: String) -> bool {
 		let mut p_stack: Vec<char> = p.chars().collect();
 		let mut s_stack: Vec<char> = s.chars().collect();
 
 		let mut p_iterator: usize = 0;
 		let mut s_iterator: usize = 0;
-		let mut current_p_symbol: char = '!';
+		let mut current_regexp_symbol: RegexpSymbol = RegexpSymbol{ symbol_type: vec![],
+		symbols: vec![], min: 0, max: 0 };
 		let mut current_s_symbol: char = '!';
-		let mut asterisk_symbol_array: Vec<char> = vec![];
+		let mut regexp_array: Vec<RegexpSymbol> = vec![];
+		let mut counter: usize = 0;
+		while p_stack.len() > 0 {
+			regexp_array.push( RegexpSymbol { symbol_type: vec![], symbols: vec![], min: 0, max: 0 } );
+			let mut current_p_symbol_local: char = p_stack.pop().expect("boom");
+			if current_p_symbol_local == '*' {
+				regexp_array[counter].min = 0;
+				regexp_array[counter].max = usize::MAX;
+				regexp_array[counter].symbol_type.push( current_p_symbol_local );
+				if p_stack.len() > 0 {
+					current_p_symbol_local = p_stack.pop().expect("boom");
+					regexp_array[counter].symbol_type.push( current_p_symbol_local );
+				}
+//				println!("element: {:?}", regexp_array[regexp_array.len() - 1].symbols);
+
+			} else {
+				regexp_array[counter].min = 1;
+				regexp_array[counter].max = 1;
+				regexp_array[counter].symbol_type.push( current_p_symbol_local );
+//				println!("TEST");
+//				println!("element: {:?}", regexp_array[regexp_array.len() - 1].symbols);
+//				println!("element: {:?}", regexp_array[regexp_array.len() - 1].max);
+			}
+
+			counter += 1;
+		}
+		
 		let mut no_machted_substring: Vec<char> = vec![];
 		let mut asterisk_symbol_array_iterator: usize = 0;
 		let mut consumed_symbols_number_after_asterisk: usize = 0;
@@ -46,161 +70,49 @@ impl Solution {
 		let mut is_p_match: bool = true;
 		let mut is_asterisk_next: bool = false;
 		let mut is_asterisk_match: bool = false;
-		
-		while p_stack.len() > 0 || s_stack.len() > 0
-		{
-			if p_stack.len() == 0 && s_stack.len() > 0 {
-				return false;
-			}
+//		println!("array: {:#?}", regexp_array);
 
-			if s_stack.len() > 0 && is_s_match {
+		while s_stack.len() > 0
+		{
+			if s_stack.len() > 0 {
 				current_s_symbol = s_stack.pop().expect("boom");
-			} else if s_stack.len() == 0 && is_s_match {
+			} else {
 				current_s_symbol = '#';
 			}
 
-			if p_stack.len() > 0 && is_p_match {
-				current_p_symbol = p_stack.pop().expect("boom");
-			} else if p_stack.len() == 0 && is_p_match {
-				current_p_symbol = '#';
-			}
-
-			if is_asterisk_next {
-				is_asterisk_next = false;
-			}
-			
-			if current_p_symbol == '*' {
-				is_asterisk_next = true;
-				if p_stack.len() > 0 {
-					current_p_symbol = p_stack.pop().expect("boom");
-				}
-			}
-
-			
-			println!("current s symbol: {}", current_s_symbol);
-			println!("current p symbol: {}", current_p_symbol);
-			println!("s length: {}", s_stack.len());
-			println!("p length: {}", p_stack.len());
-//			asterisk_symbol_array.reverse();
-			println!("AR: {:?}", asterisk_symbol_array);
-			
-			if Solution::is_symbols_correct( current_p_symbol, current_s_symbol ) {
-				if !is_asterisk_next && current_s_symbol != '#' &&
-					(current_p_symbol == current_s_symbol || current_p_symbol == '.') {
-						println!("match case");
-						is_s_match = true;
-						is_p_match = true;
-
-						if s_stack.len() == 0 {
-							current_s_symbol = '#';
+			println!("current symbol: {}", current_s_symbol);
+			let mut regexp_iterator: usize = 0;
+			let regexp_array_size: usize = regexp_array.len();
+			while regexp_iterator < regexp_array_size {
+				let current_regexp_symbol: &mut RegexpSymbol = &mut regexp_array[regexp_iterator];
+				if current_regexp_symbol.max == 1 && current_regexp_symbol.min == 1 &&
+					current_regexp_symbol.symbols.len() < current_regexp_symbol.max {
+						let current_iterator: usize = current_regexp_symbol.symbol_type.len();
+						if current_iterator > 0 &&
+							current_regexp_symbol.symbol_type[current_iterator - 1] ==
+							current_s_symbol {
+							current_regexp_symbol.symbols.push( current_s_symbol );
+							println!("current regexp symbol 0: {:?}", current_regexp_symbol.symbols);
 						}
-						if p_stack.len() == 0 {
-							current_p_symbol = '#';
-						}
-					} else if is_asterisk_next {
-						println!("* case");
-						while s_stack.len() > 0 {
-							if (current_p_symbol == current_s_symbol ||
-								current_p_symbol == '.') && current_s_symbol != '#' {
-									is_s_match = true;
-									println!("* match: {}", current_p_symbol);
-									asterisk_symbol_array.push( current_s_symbol );
-									current_s_symbol = s_stack.pop().expect("boom");
-								} else {
-									is_s_match = false;
-									break;
-								}
-						}
-
-						if (current_p_symbol == current_s_symbol ||
-							current_p_symbol == '.') && current_s_symbol != '#' {
-								is_s_match = true;
-								println!("* last match: {}", current_p_symbol);
-								asterisk_symbol_array.push( current_s_symbol );
-							} else {
-								is_s_match = false;
+						break;
+					} else if current_regexp_symbol.max == usize::MAX && current_regexp_symbol.min == 0 {
+						let current_iterator: usize = current_regexp_symbol.symbol_type.len();
+						if current_iterator > 0 && current_regexp_symbol.symbol_type[current_iterator - 1] == '.' {
+							current_regexp_symbol.symbols.push( current_s_symbol );
+							println!("current regexp symbol 1: {:?}", current_regexp_symbol.symbols);
+						} else if current_iterator > 0 &&
+							current_regexp_symbol.symbol_type[current_iterator - 1] ==
+							current_s_symbol {
+								
 							}
 
-						if s_stack.len() == 0 && is_s_match {
-							current_s_symbol = '#';
-						}
-						if p_stack.len() == 0 {
-							current_p_symbol = '#';
-						}
-						println!("* is s matched: {}", is_s_match);
-					} else {
-						println!("no match case");
-						let mut asterisk_symbol_array_temp: Vec<char> = asterisk_symbol_array.clone();
-						asterisk_symbol_array_temp.reverse();
-						no_machted_substring.push( current_p_symbol );
-						let mut no_matched_substring_temp: Vec<char> = no_machted_substring.clone();
-						no_matched_substring_temp.reverse();
-						println!("reversed array: {:?}", asterisk_symbol_array_temp);
-						println!("substring: {:?}", no_matched_substring_temp);
-						let mut is_contain_substring: bool = false;
-						let mut outer_counter: usize = asterisk_symbol_array_iterator;
-						while outer_counter < asterisk_symbol_array_temp.len() {
-							println!("iter #: {}", outer_counter);
-							let mut inner_counter: usize = 0;
-							while inner_counter < no_matched_substring_temp.len() &&
-								outer_counter + inner_counter < asterisk_symbol_array_temp.len() {
-									if no_matched_substring_temp[inner_counter] ==
-										asterisk_symbol_array_temp[outer_counter + inner_counter] ||
-										no_matched_substring_temp[inner_counter] == '.' {
-											println!("ELEM: {}", asterisk_symbol_array_temp[outer_counter + inner_counter]);
-											is_contain_substring = true;
-										} else {
-											is_contain_substring = false;
-											break;
-										}
-									inner_counter += 1;
-								}
-							if is_contain_substring {
-								break;
-							}
-							outer_counter += 1;
-						}
-						println!("is s matched: {}", is_s_match);
-						if p_stack.len() == 0 && is_contain_substring && outer_counter == 0 {
-							current_p_symbol = '#';
-						} else if is_contain_substring {
-							if current_s_symbol != '#' {
-								s_stack.push( current_s_symbol );
-								is_s_match = true;
-							}
-							let mut counter: usize = asterisk_symbol_array_iterator;
-							asterisk_symbol_array_iterator = outer_counter;
-							while counter < asterisk_symbol_array_iterator {
-								s_stack.push( asterisk_symbol_array_temp[ counter ] );
-								counter += 1;
-							}
-						}
-						println!("s_stack after fill: {:?}", s_stack);
-						
-						if !is_contain_substring {
-							return false;
-						} else if no_matched_substring_temp.len() > asterisk_symbol_array_temp.len() {
-							return false;
-						}
+						break;
 					}
-			} else {
-				println!("wrong symbol case");
+				regexp_iterator += 1;
 			}
 		}
 
-		println!("final p iterator: {}", p_iterator);
-		println!("final p elem: {}", current_p_symbol);
-
-		println!("final s iterator: {}", s_iterator);
-		println!("final s elem: {}", current_s_symbol);
-
-		println!("array: {:?}", asterisk_symbol_array);
-
-		if current_p_symbol == '#' && current_s_symbol == '#' {
-			return true;
-		} else {
-			return false;
-		}
+		true
 	}
 }
 
@@ -211,8 +123,8 @@ fn main() {
 	// let s: String = String::from("qweacbmzcab");  // 14 last index
 	// let p: String = String::from("qweacbmzca*ab.*");  // 16 last index
 
-	let s: String = String::from("aab");  // 14 last index
-	let p: String = String::from("b.*");  // 16 last index
+	let s: String = String::from("baabbbaccbccacacc");  // 14 last index
+	let p: String = String::from("c*..b*a*a.*a..*c");  // 16 last index
 
 	println!("{}", Solution::is_match( s, p ));
 }
